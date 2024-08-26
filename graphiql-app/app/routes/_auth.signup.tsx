4d510f694e6bf44a695fcb0b8d3be44ab6efe739
LeonID <lmatskevich79@gmail.com>
2024-08-27 01:35:34 +0300

    feat: add _auth.signup.tsx

--- graphiql-app/app/routes/_auth.signup.tsx
+++ graphiql-app/app/routes/_auth.signup.tsx
@@ -1,18 +1,100 @@
-import { Link } from "@remix-run/react";
-import { FieldValues, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { userSchema } from "../utils/userSchema";
+import { Form, Link } from "@remix-run/react";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import InputField from "../components/Input/Input";
+import { auth } from "../firebase";
 
-export default function Signup() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isDirty, isValid },
-  } = useForm({ resolver: yupResolver(userSchema), mode: "onChange" });
+const Register: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [user, setUser] = useState<{
+    email: string;
+    error: string;
+    emailVerified: boolean;
+  } | null>(null);
 
-  const onSubmit = async (data: FieldValues) => {
-    console.log(data);
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
+
+  const validatePassword = (password: string) => {
+    const unicodeRegex = /[\p{L}\p{N}\p{P}\p{S}]/u;
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/;
+
+    return passwordRegex.test(password) && unicodeRegex.test(password);
+  };
+
+  const handleEmailChange = (data: string) => {
+    setEmail(data);
+    if (!validateEmail(data)) {
+      setEmailError("Invalid email format");
+    } else {
+      setEmailError(null);
+    }
+  };
+
+  const handlePasswordChange = (data: string) => {
+    setPassword(data);
+    if (!validatePassword(data)) {
+      setPasswordError(
+        "Password must include at least one letter, one digit and one special symbol",
+      );
+    } else {
+      setPasswordError(null);
+    }
+  };
+
+  const signUpAction = async () => {
+    if (emailError || passwordError) return;
+
+    try {
+      const userCred = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCred.user;
+
+      await sendEmailVerification(user);
+      console.log("Success");
+
+      setEmail("");
+      setPassword("");
+
+      window.location.reload();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error during registration:", error.message);
+        setPasswordError("Registration failed. Please try again.");
+      } else {
+        console.error("Unexpected error:", error);
+        setPasswordError("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userCred) => {
+      if (userCred) {
+        const { email, emailVerified } = userCred;
+        setUser({
+          email: email ?? "",
+          emailVerified: emailVerified ?? false,
+          error: "",
+        });
+      } else {
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <h2 className="text-center text-3xl m-2">Sign Up</h2>
@@ -22,41 +104,63 @@ export default function Signup() {
           Sign In!
         </Link>
       </p>
-      <form
+      <Form
         className="flex flex-col items-center"
-        onSubmit={handleSubmit(onSubmit)}
+        action="/signup"
+        method="post"
       >
-        <div className="flex justify-center m-2">
-          <label className="text-2xl" htmlFor="email">
-            Email
-          </label>
-          <input
-            {...register("email")}
-            className="border-solid border-2 rounded-2xl ml-11 px-2"
-            type="email"
-            formNoValidate={true}
-          />
-        </div>
-        <div className="text-red-500 min-h-6">{errors.email?.message}</div>
-        <div className="flex justify-center m-2">
-          <label className="text-2xl" htmlFor="password">
-            Password
-          </label>
-          <input
-            {...register("password")}
-            className="border-solid border-2 rounded-2xl mx-2 px-2"
-            type="password"
-          />
-        </div>
-        <div className="text-red-500 min-h-10">{errors.password?.message}</div>
-        <button
-          disabled={!isDirty || !isValid}
-          className="text-2xl mt-4 border-solid rounded-3xl bg-gray-500 p-4 bg-gradient-to-tl from-gray-300 via-gray-500 to-black text-center align-self-center"
-          type="submit"
+        <div
+          className={`w-80 ${emailError ? "error" : email ? "success" : ""}`}
         >
-          Submit
+          <div className="flex justify-between m-2 w-80">
+            <label className="text-2xl" htmlFor="email">
+              Email
+            </label>
+            <InputField
+              placeholder={"E-mail Address"}
+              handleChange={handleEmailChange}
+              type={"email"}
+            />
+          </div>
+          {emailError && (
+            <div className="text-red-500 text-xs mt-2">{emailError}</div>
+          )}
+        </div>
+        <div
+          className={`w-80 ${passwordError ? "error" : password ? "success" : ""}`}
+        >
+          <div className="flex justify-between m-2 w-80">
+            <label className="text-2xl" htmlFor="password">
+              Password
+            </label>
+            <InputField
+              placeholder={"Password"}
+              handleChange={handlePasswordChange}
+              type={"password"}
+            />
+          </div>
+          {passwordError && (
+            <div className="text-red-500 text-xs mt-2">{passwordError}</div>
+          )}
+        </div>
+        <button
+          className="text-2xl mt-4 border-solid rounded-3xl bg-gray-500 p-4 bg-gradient-to-tl from-gray-300 via-gray-500 to-black text-center align-self-center"
+          type="button"
+          onClick={signUpAction}
+        >
+          Sign Up
         </button>
-      </form>
+      </Form>
+      {user && (
+        <div className="text-sm flex flex-col items-center mt-10">
+          <p>{user?.email}</p>
+          <p>
+            {user?.emailVerified ? "Email verified!" : "Email not verified!"}
+          </p>
+        </div>
+      )}
     </>
   );
-}
+};
+
+export default Register;
