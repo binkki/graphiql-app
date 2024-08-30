8ccd53dc59c93eb5851aac85bdaa781dfbebeea4
jofetta <jafarovatatiana@gmail.com>
2024-08-30 15:32:42 +0300

    feat: add language options in the header

--- graphiql-app/app/components/Header.tsx
+++ graphiql-app/app/components/Header.tsx
@@ -1,28 +1,29 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import { useTranslation } from "react-i18next";
-import { loader } from "../routes/_index";
+import i18nextOptions from "../i18nextOptions";
 
 export const handle = {
   i18n: ["common"],
 };
 
 export default function Header() {
-  const { lngs } = useLoaderData<typeof loader>();
+  const lngs = i18nextOptions.supportedLngs;
   const { ready, i18n } = useTranslation();
+  const location = useLocation();
 
   if (!ready) return <div>Loading...</div>;
   return (
     <header className="flex justify-between h-16 p-4 sticky text-3xl text-white bg-gradient-to-t from-gray-300 to-black">
       <h1>Graphiql App</h1>
       <div>
-        {Object.keys(lngs).map((lng) => (
+        {lngs.map((lng) => (
           <Link
             key={lng}
             style={{
               marginRight: 5,
               fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
             }}
-            to={`/?lng=${lng}`}
+            to={`${location.pathname}?lng=${lng}`}
           >
             {lng.toUpperCase()}
           </Link>
--- graphiql-app/app/routes/_auth/index.tsx
+++ graphiql-app/app/routes/_auth/index.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "@remix-run/react";
-// import Header from "../../components/Header";
+import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 export default function AuthIndex() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <h1 className="text-4xl text-center m-4">Welcome</h1>
       <div className="flex flex-col mx-auto ">
         <Outlet />
