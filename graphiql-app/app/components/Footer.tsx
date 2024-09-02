cb16c0b5e0beb5eeb4d2989b42422dfc2b0e72bb
LeonID <lmatskevich79@gmail.com>
2024-09-02 22:04:29 +0300

    feat: add hover

--- graphiql-app/app/components/Footer.tsx
+++ graphiql-app/app/components/Footer.tsx
@@ -7,7 +7,12 @@ export default function Footer() {
     <footer className="flex h-16 p-4 sticky top-full text-3xl text-white bg-gradient-to-b from-gray-300 to-black">
       <div className="flex items-center gap-5 w-screen justify-between">
         <div>
-          <Link className="inline-block" to="//github.com/binkki/graphiql-app">
+          <Link
+            className="inline-block hover:scale-105 transition-transform duration-200"
+            to="//github.com/binkki/graphiql-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={github} className="w-10 h-10" alt="GitHub logo" />
           </Link>
         </div>
@@ -15,7 +20,12 @@ export default function Footer() {
           <p className="inline-block text-base">2024</p>
         </div>
         <div>
-          <Link className="inline-block" to="//rs.school/courses/reactjs">
+          <Link
+            className="inline-block hover:scale-105 transition-transform duration-200"
+            to="//rs.school/courses/reactjs"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={rs} className="w-10 h-10" alt="RS School logo" />
           </Link>
         </div>
