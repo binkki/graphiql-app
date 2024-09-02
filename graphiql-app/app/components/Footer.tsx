1c3cf3ae58d9f385ca6d15bb466e57ab7e6ab8ab
LeonID <lmatskevich79@gmail.com>
2024-09-02 06:49:37 +0300

    feat: add logo

--- graphiql-app/app/components/Footer.tsx
+++ graphiql-app/app/components/Footer.tsx
@@ -1,7 +1,25 @@
+import { Link } from "@remix-run/react";
+import github from "../../public/github-mark.svg";
+import rs from "../../public/rss-logo.svg";
+
 export default function Footer() {
   return (
     <footer className="flex h-16 p-4 sticky top-full text-3xl text-white bg-gradient-to-b from-gray-300 to-black">
-      <h1>Footer</h1>
+      <div className="flex items-center gap-5 w-screen justify-between">
+        <div>
+          <Link className="inline-block" to="//github.com/binkki/graphiql-app">
+            <img src={github} className="w-10 h-10" alt="GitHub logo" />
+          </Link>
+        </div>
+        <div>
+          <p className="inline-block text-base">2024</p>
+        </div>
+        <div>
+          <Link className="inline-block" to="//rs.school/courses/reactjs">
+            <img src={rs} className="w-10 h-10" alt="RS School logo" />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
