938f103ba9d8b6f86e57b3a281c13ffaaa080fb4
jofetta <jafarovatatiana@gmail.com>
2024-09-03 18:11:17 +0300

    feat: add sign in/sign up/ sign out/main page buttons to header

--- graphiql-app/app/components/Header.tsx
+++ graphiql-app/app/components/Header.tsx
@@ -1,8 +1,10 @@
 import { Link, useLocation } from "@remix-run/react";
 import { useTranslation } from "react-i18next";
 import i18nextOptions from "../i18nextOptions";
-// import { onAuthStateChanged, User } from "firebase/auth";
-// import { auth } from "~/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "~/firebase";
+import { useState, useEffect } from "react";
+import UserSingOut from "./UserSingOut/UserSingOut";
 
 export const handle = {
   i18n: ["common"],
@@ -10,14 +12,26 @@ export const handle = {
 
 export default function Header() {
   const lngs = i18nextOptions.supportedLngs;
-  const { ready, i18n } = useTranslation();
+  const { t, ready, i18n } = useTranslation();
   const location = useLocation();
+  const [authUser, setAuthUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => listen();
+  }, []);
 
   if (!ready) return <div>Loading...</div>;
   return (
-    <header className="flex justify-between h-16 p-4 sticky text-3xl text-white bg-gradient-to-t from-gray-300 to-black">
-      <h1>Graphiql App</h1>
-      <div>
+    <header className="flex flex-wrap justify-between content-center h-24 p-4 sticky text-3xl text-white bg-gradient-to-t from-gray-300 to-black">
+      <h1 className="p-8">Graphiql App</h1>
+      <div className="p-8">
         {lngs.map((lng) => (
           <Link
             key={lng}
@@ -31,6 +45,32 @@ export default function Header() {
           </Link>
         ))}
       </div>
+      {!authUser ? (
+        <div className="flex">
+          <Link
+            className="text-2xl m-4 border-solid rounded-3xl bg-gray-500 p-4 bg-gradient-to-tl from-gray-300 via-gray-500 to-black text-center align-self-center hover:bg-gradient-to-tr hover:from-black hover:via-gray-500 hover:to-gray-300"
+            to="/signin"
+          >
+            {t("signin")}
+          </Link>
+          <Link
+            className="text-2xl m-4 border-solid rounded-3xl bg-gray-500 p-4 bg-gradient-to-tl from-gray-300 via-gray-500 to-black text-center align-self-center hover:bg-gradient-to-tr hover:from-black hover:via-gray-500 hover:to-gray-300"
+            to="/signup"
+          >
+            {t("signup")}
+          </Link>
+        </div>
+      ) : (
+        <div className="flex">
+          <Link
+            className="text-2xl m-4 border-solid rounded-3xl bg-gray-500 p-4 bg-gradient-to-tl from-gray-300 via-gray-500 to-black text-center align-self-center hover:bg-gradient-to-tr hover:from-black hover:via-gray-500 hover:to-gray-300"
+            to="/"
+          >
+            {t("home_page")}
+          </Link>
+          <UserSingOut />
+        </div>
+      )}
     </header>
   );
 }
