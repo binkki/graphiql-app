b9e3947cb9b62027dde379c08ebd92f85f91a938
LeonID <lmatskevich79@gmail.com>
2024-09-06 01:16:37 +0300

    feat: add default font size

--- graphiql-app/tailwind.config.ts
+++ graphiql-app/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: "16px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
