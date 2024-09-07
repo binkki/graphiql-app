9624c064d6e1cbc95e69ad94aa02b1218f0b4ed7
LeonID <lmatskevich79@gmail.com>
2024-09-07 21:51:59 +0300

    feat: add smooth scrolling

--- graphiql-app/tailwind.config.ts
+++ graphiql-app/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      scrollBehavior: {
+        smooth: "smooth",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
