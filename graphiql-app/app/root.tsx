d9c9822ae312b64a72f18a6388111872412e9c6c
LeonID <lmatskevich79@gmail.com>
2024-09-15 18:55:08 +0300

    fix: fix header scrol

--- graphiql-app/app/root.tsx
+++ graphiql-app/app/root.tsx
@@ -47,7 +47,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="h-screen scroll-smooth">
+      <body className="scroll-smooth">
         <AuthCheck>
           <Header />
           {children}
