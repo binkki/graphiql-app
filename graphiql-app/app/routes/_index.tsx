d636e68d2af813d03784eee9046e5e522bc29f1b
jofetta <jafarovatatiana@gmail.com>
2024-09-15 19:54:40 +0300

    add developers info and github links

--- graphiql-app/app/routes/_index.tsx
+++ graphiql-app/app/routes/_index.tsx
@@ -57,6 +57,16 @@ export default function Index() {
               </li>
             </ul>
           </div>
+          <div className="text-center text-xl font-small mb-5">
+            {t("Developed")}{" "}
+            <Link to={"https://rs.school/courses/reactjs"}>{t("course")}</Link>{" "}
+            {t("by")}
+          </div>
+          <div className="flex justify-center mb-[6.25rem] text-xl text-blue-500 gap-10">
+            <Link to={"https://github.com/binkki"}>{t("Kate")}</Link>
+            <Link to={"https://github.com/Jofetta"}>{t("Tania")}</Link>
+            <Link to={"https://github.com/SpiriT-L"}>{t("Leonid")}</Link>
+          </div>
         </>
       ) : (
         <>
