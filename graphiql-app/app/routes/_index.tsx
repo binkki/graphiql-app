6a11f9cb8161ef96b54d6dee9437e65e9160063e
jofetta <jafarovatatiana@gmail.com>
2024-09-15 20:56:19 +0300

    fix: fix dev info

--- graphiql-app/app/routes/_index.tsx
+++ graphiql-app/app/routes/_index.tsx
@@ -3,6 +3,7 @@ import { Link } from "@remix-run/react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import DevInfo from "~/components/devInfo";
 import { auth } from "~/firebase";
 
 export const meta: MetaFunction = () => {
@@ -57,16 +58,7 @@ export default function Index() {
               </li>
             </ul>
           </div>
-          <div className="text-center text-xl font-small mb-5">
-            <span>{t("Developed_by")}</span>{" "}
-            <Link to={"https://rs.school/courses/reactjs"}>{t("course")}</Link>{" "}
-            <span>{t("by")}</span>
-          </div>
-          <div className="flex justify-center mb-[6.25rem] text-xl text-blue-500 gap-10">
-            <Link to={"https://github.com/binkki"}>{t("Kate")}</Link>
-            <Link to={"https://github.com/Jofetta"}>{t("Tania")}</Link>
-            <Link to={"https://github.com/SpiriT-L"}>{t("Leonid")}</Link>
-          </div>
+          <DevInfo />
         </>
       ) : (
         <>
@@ -92,6 +84,7 @@ export default function Index() {
               </Link>
             </div>
           </div>
+          <DevInfo />
         </>
       )}
     </>
